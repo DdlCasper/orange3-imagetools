d6ba6dd48bd696b9e50064c3d8c106e94523950a
DdlCasper <riahi.anur@gmail.com>
2026-08-11 11:49:48 +0200

    Added _init_ file to initizalize preprocessor into registry

---
+++ orangecontrib/imagetools/preprocess_snom/__init__.py
@@ -0,0 +1,5 @@
+from orangecontrib.imagetools.preprocess_snom import spatial_filter  # noqa: F401
+from orangecontrib.imagetools.preprocess_snom import binarization  # noqa: F401
+from orangecontrib.imagetools.preprocess_snom import gradient_watershed  # noqa: F401
+from orangecontrib.imagetools.preprocess_snom import labelling  # noqa: F401
+from orangecontrib.imagetools.preprocess_snom import morphology_editor  # noqa: F401
