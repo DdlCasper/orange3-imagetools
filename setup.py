4176e54cef59a13fd5d26c7063ca59141b49ea77
DdlCasper <riahi.anur@gmail.com>
2026-08-11 11:24:39 +0200

    Version name added

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="orange3-imagetools",
+    version = "0.1.0"
     packages=find_packages(),
     entry_points={
         'orange.widgets': [
