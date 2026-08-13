57ba89421dda53f13be68143f4f9ed6a41b0f055
DdlCasper <riahi.anur@gmail.com>
2026-08-13 11:36:47 +0200

    removed folder (fourth try)

--- orangecontrib/imagetools/preprocess_snom/__init__.py
+++
@@ -1,5 +0,0 @@
-from orangecontrib.imagetools.preprocess_snom import spatial_filter  # noqa: F401
-from orangecontrib.imagetools.preprocess_snom import binarization  # noqa: F401
-from orangecontrib.imagetools.preprocess_snom import gradient_watershed  # noqa: F401
-from orangecontrib.imagetools.preprocess_snom import labelling  # noqa: F401
-from orangecontrib.imagetools.preprocess_snom import morphology_editor  # noqa: F401
